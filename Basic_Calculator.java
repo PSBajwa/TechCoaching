e98c99472afd33175e1d9ecbf5a603d2c7786646
Parvinder Singh <bajwa78@gmail.com>
2020-12-11 11:48:12 +0530

    first commit

---
+++ Basic_Calculator.java
@@ -0,0 +1,120 @@
+import java.util.Scanner;
+
+public class Basic_Calculator {
+
+    static String[] calculatorOptions ={"Addition", "Subtraction", "Multiplication", "Division"};
+    
+    private double intAddition(double numX, double numY)
+    {
+        return numX + numY;
+    }
+    private double intSubtraction(double numX, double numY)
+    {
+        return numX - numY;
+    }
+    private double intMultiplication(double numX, double numY)
+    {
+        return numX *  numY;
+    }
+    private double intDivision(double numX, double numY)
+    {
+        return numX / numY;
+    }
+    public void displayCalculatorMenu()
+    {
+        for (int i = 0; i < calculatorOptions.length; i++)
+        {
+            System.out.println((i+1) + ". " + calculatorOptions[i]);
+        }
+        System.out.print("Please enter your choice: ");
+    }
+    public boolean initiateCalculation(int menuChoice)
+    {
+        if (menuChoice>=1 && menuChoice<=calculatorOptions.length)
+        {
+            System.out.println("Proceed with : " + calculatorOptions[menuChoice-1]);
+            return true;
+        } else {
+            System.out.print("Please enter a valid option : ");
+            return false;
+        }
+
+    }
+    public double executeCalculation(int calculationChoice, double numX, double numY)
+    {
+        switch(calculationChoice) {
+            case 1:
+            return intAddition(numX, numY);
+            case 2:
+            return intSubtraction(numX, numY);
+            case 3:
+            return intMultiplication(numX, numY);
+            case 4:
+            return intDivision(numX, numY);
+            default:
+            return 0.0d;
+        }    
+    }
+    public double verifyInput(String inputValue)
+    {
+        double vInput = -0.0d;
+        if (inputValue == null) {
+            return vInput;
+        }
+        try {
+              vInput = Double.parseDouble(inputValue);
+        } catch (NumberFormatException nfe) {
+            return vInput;
+        }
+        return vInput;
+    }
+    public static void main(String[] args) {
+        Basic_Calculator myCalculator = new Basic_Calculator();
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        boolean startCalculation = false;
+        boolean verifyNum = false;
+        String choice = "0";
+        int choiceNum = 0;
+        double numX = 0.0d, numY = 0.0d;
+        
+        myCalculator.displayCalculatorMenu();
+                
+        while (!startCalculation)
+        {   
+            choice = myObj.nextLine(); // Read user input
+            choiceNum = (int)myCalculator.verifyInput(choice);
+            startCalculation = myCalculator.initiateCalculation(choiceNum);
+        }
+
+        System.out.print("Please enter first number: ");
+        while (!verifyNum)
+        {
+        numX = myCalculator.verifyInput(myObj.nextLine());
+        verifyNum = true;
+        if (numX == -0.0d)
+        {
+            System.out.print("Please enter a valid number : ");
+            numX = 0.0d;
+            verifyNum = false;
+        }
+        }
+        
+        verifyNum = false;
+        System.out.print("Please enter second number: ");
+        while (!verifyNum)
+        {
+        numY = myCalculator.verifyInput(myObj.nextLine());
+        verifyNum = true;
+        if (numY == -0.0d)
+        {
+            System.out.print("Please enter a valid number : ");
+            numY = 0.0d;
+            verifyNum = false;
+        }
+        }
+        
+        System.out.println("The result of calculation is: " + myCalculator.executeCalculation(choiceNum, numX, numY));
+        myObj.close();
+      }
+
+}
